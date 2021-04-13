6f61b22cb7612d65ca55111383f38613f31248c0
qassandrach <qassiechadir@gmail.com>
2021-04-13 21:06:24 +0700

    views

--- app/core/App.php
+++ app/core/App.php
@@ -1,15 +1,50 @@
 <?php
 
 class App {
+
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $params = [];
+    // create constructor
     public function __construct()
     {
         $url = $this->parseURL();
-        var_dump($url);
+
+        //controller
+        if(isset($url[0])){
+            if(file_exists('../app/controller/' . $url[0] . '.php')){
+                $this->controller = $url[0];
+                unset($url[0]);
+            }
+        }
+
+        require_once '../app/controller/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        //method
+        if(isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+        //params
+        if(!empty($url)){
+            $this->params = array_values($url);
+
+        }
+
+        //run method and controller if exist, and send paramaters if exist
+        call_user_func_array([$this->controller, $this->method], $this->params);
+
     }
 
+    // splitting url into array
     public function parseURL() {
         if (isset($_GET['url'])){
-            $url = $_GET['url'];
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
     }
---
+++ app/views/templates/header.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+<header>
+        <nav>
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Data Sekolah</a></li>
+                <li><a href="#">Data Kriteria</a></li>
+                <li><a href="#">Prioritas Sekolah</a></li>
+            </ul>
+        </nav>
+    </header>

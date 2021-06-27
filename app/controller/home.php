749cedd94ad9404fa14761d7390d34d85397b169
Rayhan <rayhan@badr.co.id>
2021-06-27 18:51:15 +0700

    fix bug authentication

--- app/controller/home.php
+++ app/controller/home.php
@@ -1,17 +1,24 @@
 <?php
+session_start();
 
-class Home extends Controller {
+class Home extends Controller
+{
+    public function __construct()
+    {
+        if (!$_SESSION['username']) {
+            header("Location: " . BASEURL . "/login/logout");
+        }
+    }
+
     public function index()
     {
         $data['judul'] = 'Home';
-        // $data['nama'] = $this->model('user_model')->getUser();
         $data['sekolah'] = $this->model('sekolah_model')->getAllSekolah();
         $data['kriteria'] = $this->model('kriteria_model')->getAllKriteria();
-        
+
         $this->view('home/jumbotron', $data);
-       $this->view('templates/header', $data);
-       $this->view('home/index', $data);
-       $this->view('templates/footer');
+        $this->view('templates/header', $data);
+        $this->view('home/index', $data);
+        $this->view('templates/footer');
     }
-    
 }

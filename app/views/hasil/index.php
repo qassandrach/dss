1cea8c776f5ccc80f50054179018ff9849fe00ee
qassandrach <qassiechaidir@gmail.com>
2021-07-19 10:20:24 +0700

    Show table

--- app/views/hasil/index.php
+++ app/views/hasil/index.php
@@ -24,16 +24,65 @@
 
         </div>
 
-    </div>
-
-    <div class="form-group">
-       
         <div class="form-input">
             <button type="submit" class="btn">Tambah</button>
         </div>
 
     </div>
 
+    <!-- <div class="form-group">
+
+        <div class="form-input">
+            <button type="submit" class="btn">Tambah</button>
+        </div>
+
+    </div> -->
+
+
+
+    <div class="card">
+        <?php
+        $sekolah = array();
+        for ($i = 1; $i <= count($data['sekolah']); $i++) {
+            $sekolah[$i] = $data['sekolah'][$i - 1]['nama_sekolah'];
+        }
+        ?>
+        <table>
+            <h4>Ranking</h4>
+            <thead>
+                <tr>
+                    <th>Ranking</th>
+                    <th>Id Sekolah</th>
+                    <th>Sekolah</th>
+                    <th>Nilai Preferensi (V<sub>i</sub>)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $k = 0;
+                $matrix7 = $data['result'];
+                arsort($matrix7);
+                foreach ($matrix7 as $key => $value) {
+                    echo "<tr>
+                       <td>" . (++$k) . "</td>
+                       <td>" . $key . "</td>
+                       <td>" . $sekolah[$key] . "</td>
+                       <td>" . $value . "</td>
+                       </tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
 
 </form>
+
+<?php if ($_SESSION && $_SESSION['role'] === 'Administrator' && count($sekolah) > 0 ) { ?>
+
+    <button><a href="<?= BASEURL; ?>/hasil/proses/">Lihat Proses</a></button>
+
+<?php } ?>
+<button id="simpan" type="submit">Simpan Data</button>
+        <a class="batal" href="<?= BASEURL; ?>/sekolah/index" class="btn btn-light">Clear</a>
 
